6a4db03e1422b96cc73d469992e3939f12672b37
Leandro Alves Santos <lealsto82@gmail.com>
2018-07-22 17:34:03 -0300

    Organized folder structure

---
+++ IOSND-VirtualTourist/IOSND-VirtualTourist/Model/Data.swift
@@ -0,0 +1,9 @@
+//
+//  Data.swift
+//  IOSND-VirtualTourist
+//
+//  Created by Leandro Alves Santos on 22/07/18.
+//  Copyright © 2018 Leandro Alves Santos. All rights reserved.
+//
+
+import Foundation
