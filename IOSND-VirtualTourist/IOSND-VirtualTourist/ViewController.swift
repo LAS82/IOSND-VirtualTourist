5cc411ff29cf8f28bab2a1db84d1979a3b72fa26
Leandro Alves Santos <lealsto82@gmail.com>
2018-07-22 15:41:19 -0300

    Clean Project

---
+++ IOSND-VirtualTourist/IOSND-VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSND-VirtualTourist
+//
+//  Created by Leandro Alves Santos on 22/07/18.
+//  Copyright © 2018 Leandro Alves Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
