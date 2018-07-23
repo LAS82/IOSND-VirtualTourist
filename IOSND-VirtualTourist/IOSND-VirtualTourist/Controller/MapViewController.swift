70afe12521960237895246d5350050f533e747d7
Leandro Alves Santos <lealsto82@gmail.com>
2018-07-22 23:06:28 -0300

    Map view partially completed. Includes set a pin on map and also, record pin data on the database an navigates to the next screen

--- IOSND-VirtualTourist/IOSND-VirtualTourist/Controller/MapViewController.swift
+++ IOSND-VirtualTourist/IOSND-VirtualTourist/Controller/MapViewController.swift
@@ -7,22 +7,90 @@
 //
 
 import UIKit
+import CoreData
 import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
     
     var dataController: DataController!
-
+    var mapPress = UILongPressGestureRecognizer()
+    let fetch: NSFetchRequest<Pin> = Pin.fetchRequest()
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mapPress = UILongPressGestureRecognizer(target: self, action: #selector(setPinOnMap(sender:)))
+        
+        map.addGestureRecognizer(mapPress)
+        
+        loadPinsOnMap()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func setPinOnMap(sender: UILongPressGestureRecognizer) {
+        
+        let touchPoint = sender.location(in: map)
+        let touchCoordinate = map.convert(touchPoint, toCoordinateFrom: map)
+        
+        let pointAnnotation = MKPointAnnotation()
+        pointAnnotation.coordinate = touchCoordinate
+        map.addAnnotation(pointAnnotation)
+        
+        savePinData(annotation: pointAnnotation)
+        
     }
-
+    
+    func savePinData(annotation: MKPointAnnotation) {
+        
+        let newPin = Pin(context: self.dataController.viewContext)
+        newPin.latitude = annotation.coordinate.latitude as Double
+        newPin.longitude = annotation.coordinate.longitude as Double
+        
+        try? self.dataController.viewContext.save()
+        
+    }
+    
+    func setPinOnMap(_ pin: Pin) {
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(pin.latitude), longitude: CLLocationDegrees(pin.longitude))
+        
+        map.addAnnotation(annotation)
+        
+    }
+    
+    func loadPinsOnMap() {
+        if let pins = try? dataController.viewContext.fetch(fetch) {
+            
+            for pin in pins {
+                setPinOnMap(pin)
+            }
+            
+        }
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let identifier = "travelPin"
+        var pin = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
+        if pin == nil {
+            pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            pin!.canShowCallout = false
+        } else {
+            pin!.annotation = annotation
+        }
+        
+        return pin
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        performSegue(withIdentifier: "Photos", sender: self)
+    }
 
 }
 
